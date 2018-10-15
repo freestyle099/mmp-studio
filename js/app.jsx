083499b57f8446fc6ea6f31f60fb3c80e88be947
freestyle09 <jozefrzadkosz09@gmail.com>
2018-10-15 23:45:11 +0200

    updated navigation

--- js/app.jsx
+++ js/app.jsx
@@ -6,10 +6,23 @@ document.addEventListener("DOMContentLoaded", function() {
   class MMPStudio extends React.Component {
     render() {
       return (
-        <div>
-          <NavLink to="/galeria">Galeria</NavLink>
-        </div>
+        <HashRouter>
+          <div>
+            <NavLink to="/galeria">Galeria</NavLink>
+
+            <Switch>
+              <Route exact path="/home" component={MMPStudioMain} />
+              <Route path="/galeria" component={Gallery} />
+            </Switch>
+          </div>
+        </HashRouter>
       );
+    }
+  }
+
+  class MMPStudioMain extends React.Component {
+    render() {
+      return <div>Main FB</div>;
     }
   }
 
@@ -22,23 +35,21 @@ document.addEventListener("DOMContentLoaded", function() {
   class MMPStudioFotoBudka extends React.Component {
     render() {
       return (
-        <div>
-          <FotobudkaNavigation />
-          <Switch>
-            <Route exact path="/fotobudka/home" component={FBMain} />
-          </Switch>
-        </div>
+        <HashRouter>
+          <div>
+            <FotobudkaNavigation />
+            <Switch>
+              <Route exact path="/fotobudka/home" component={FBMain} />
+            </Switch>
+          </div>
+        </HashRouter>
       );
     }
   }
 
   class FotobudkaNavigation extends React.Component {
     render() {
-      return (
-        <div>
-          <div>Siema</div>
-        </div>
-      );
+      return <div>Siema</div>;
     }
   }
 

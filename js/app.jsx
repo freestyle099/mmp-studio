68bf9cbb3ca6183928adfc8b3e7893c23d620068
freestyle09 <jozefrzadkosz09@gmail.com>
2018-10-15 23:11:47 +0200

    added simply navigation

--- js/app.jsx
+++ js/app.jsx
@@ -1,8 +1,58 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom";
+import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
 
 document.addEventListener("DOMContentLoaded", function() {
-  const App = () => <h1>Hello</h1>;
 
-  ReactDom.render(<App />, document.getElementById("app"));
+  
+
+  class MMPStudio extends React.Component {
+    render() {
+      return <div>Hejka</div>;
+    }
+  }
+
+  class FBMain extends React.Component {
+    render() {
+      return <div>Siema fb Main</div>;
+    }
+  }
+
+  class MMPStudioFotoBudka extends React.Component {
+    render() {
+      return (
+        <div>
+          <FotobudkaNavigation />
+          <Switch>
+            <Route exact path="/fotobudka/home" component={FBMain} />
+          </Switch>
+        </div>
+      );
+    }
+  }
+
+  class FotobudkaNavigation extends React.Component {
+    render() {
+      return (
+        <div>
+          <div>Siema</div>
+        </div>
+      );
+    }
+  }
+
+  const App = () => {
+    return (
+      <HashRouter>
+        <div>
+          <Switch>
+            <Route exact path="/" component={MMPStudio} />
+            <Route path="/fotobudka" component={MMPStudioFotoBudka} />
+          </Switch>
+        </div>
+      </HashRouter>
+    );
+  };
+
+  ReactDOM.render(<App />, document.getElementById("app"));
 });

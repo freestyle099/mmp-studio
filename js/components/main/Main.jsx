adf390cee55403bba6ea15a1070ede4e5953554a
freestyle09 <jozefrzadkosz09@gmail.com>
2018-10-17 09:17:51 +0200

    add slider react

--- js/components/main/Main.jsx
+++ js/components/main/Main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import MMPStudioHeader from "../main/Navigation";
+import Slider from "../main/Slider";
 
 export default class MMPStudio extends React.Component {
   render() {
     return (
       <div>
         <MMPStudioHeader />
+        <Slider/>
         <div className="container">
           Home
           <NavLink to="/fotobudka">Fotobudka</NavLink>

06c111464b9f38d71b5934599e38a2302847827a
freestyle09 <jozefrzadkosz09@gmail.com>
2018-10-23 17:44:22 +0200

    done refactor for express

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
 const express = require("express");
 const app = express();
 
-const obj = [
-  {
-    name: "Zdzisiek",
-    age: 26
-  },
-  {
-    name: "Franek",
-    age: 29
-  }
-];
-
-app.get("/api/dane", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.send(obj);
+app.get("/", (req, res) => {
+  res.send("Hi There!");
 });
 
-app.listen(3000, () => console.log("Listening on port 5001..."));
+app.get("/api/dane", (req, res) => {
+  res.send([{ id: 1, name: "Franek" }, { id: 2, name: "Jontek" }]);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));

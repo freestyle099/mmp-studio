4e7def693ef5f2105064f736eabbc34bce3368d0
freestyle09 <jozefrzadkosz09@gmail.com>
2018-10-24 22:26:37 +0200

    fixed router problem

--- client/src/index.js
+++ client/src/index.js
@@ -12,33 +12,147 @@ import {
 
 import "./style/style.scss";
 
+class NavigationImages extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <ul>
+          <li>
+            <Link to="/">MMPStudio</Link>
+          </li>
+          <li>
+            <Link to="/fotobudka">Fotobudka</Link>
+          </li>
+          <li>
+            <Link to="/jubiler">Jubiler</Link>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+class FBNavigation extends React.Component {
+  render() {
+    return (
+      <nav className="fb-nav">
+        <div className="nav-wrapper container">
+          <Link className="brand-logo" to="/fotobudka/">
+            <img src="logo_fotobudka.png" alt="" />
+          </Link>
+          <ul className="container navigation right">
+            <li>
+              <Link to="/fotobudka/">Strona główna</Link>
+            </li>
+            <li>
+              <Link to="/fotobudka/kontakt">Kontakt</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+class JubilerNavigation extends React.Component {
+  render() {
+    return (
+      <nav className="jubiler-nav">
+        <div className="nav-wrapper container">
+          <Link className="brand-logo" to="/jubiler">
+            <img src={"logo_jubiler.png" || "logo_studio.png"} alt="" />
+          </Link>
+          <ul className="container navigation right">
+            <li>
+              <Link to="/jubiler">Jubiler</Link>
+            </li>
+            <li>
+              <Link to="/jubiler/galeria">Galeria</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+// Navigation
+class Navigation extends React.Component {
+  render() {
+    return (
+      <div>
+        <nav className="main-nav">
+          <div className="nav-wrapper container">
+            <Link className="brand-logo" to="/">
+              <img src="./logo_studio.png" alt="" />
+            </Link>
+            <ul className="container navigation right">
+              <li>
+                <Link to="/">Strona Domowa</Link>
+              </li>
+              <li>
+                <Link to="/galeria">Galeria</Link>
+              </li>
+              <li>
+                <Link to="/kontakt">Kontakt</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
 // Main MMPStudio
 class MMPStudio extends React.Component {
   render() {
     return (
       <div>
+        <Navigation />
         <div className="container">MMPStudio</div>
+        <NavigationImages />
       </div>
     );
   }
 }
+
 class Contact extends React.Component {
   render() {
-    return <div className="container">Kontakt</div>;
+    return (
+      <div>
+        <Navigation />
+        <div className="container">Kontakt</div>
+        <NavigationImages />
+      </div>
+    );
   }
 }
+
 class Gallery extends React.Component {
   render() {
-    return <div className="container">Galeria</div>;
+    return (
+      <div>
+        <Navigation />
+        <div className="container">Galeria</div>
+        <NavigationImages />
+      </div>
+    );
   }
 }
 
 // Fotobudka
 class Fotobudka extends React.Component {
   render() {
-    return <div>Hello Fotobudka</div>;
+    return (
+      <div>
+        <FBNavigation />
+        Hello Fotobudka
+      </div>
+    );
   }
 }
+
 class FotobudkaContact extends React.Component {
   render() {
     return <div>Hello Fotobudka Kontakt</div>;
@@ -59,169 +173,36 @@ class JubilerGallery extends React.Component {
   }
 }
 
-// Navigation
-class Navigation extends React.Component {
-  render() {
-    return (
-      <div>
-        {this.props.navigation === "main" && (
-          <nav className="main-nav">
-            <div className="nav-wrapper container">
-              <Link className="brand-logo" to="/">
-                <img src="./logo_studio.png" alt="" />
-              </Link>
-              <ul className="container navigation right">
-                <li>
-                  <Link to="/">Strona Domowa</Link>
-                </li>
-                <li>
-                  <Link to="/galeria">Galeria</Link>
-                </li>
-                <li>
-                  <Link to="/kontakt">Kontakt</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
-        )}
-        {this.props.navigation === "fb" && (
-          <nav className="fb-nav">
-            <div className="nav-wrapper container">
-              <Link className="brand-logo" to="/fotobudka">
-                <img src="logo_fotobudka.png" alt="" />
-              </Link>
-              <ul className="container navigation right">
-                <li>
-                  <Link to="/fotobudka">Strona główna</Link>
-                </li>
-                <li>
-                  <Link to="/fotobudka/kontakt">Kontakt</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
-        )}
-        {/* Jubiler Navigation */}
-        {this.props.navigation === "jubiler" && (
-          <nav className="jubiler-nav">
-            <div className="nav-wrapper container">
-              <Link className="brand-logo" to="/jubiler">
-                <img src={"logo_jubiler.png" || "logo_studio.png"} alt="" />
-              </Link>
-              <ul className="container navigation right">
-                <li>
-                  <Link to="/jubiler">Jubiler</Link>
-                </li>
-                <li>
-                  <Link to="/jubiler/galeria">Galeria</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
-        )}
-      </div>
-    );
-  }
-}
-
 class NotFound extends React.Component {
   render() {
     return <div>Not Found</div>;
   }
 }
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div>
-
-      </div>
-    );
-  }
-}
-
 // Main APP
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      navigation: "main"
-    };
-  }
-
-  changeState = e => {
-    this.setState({
-      navigation: e.target.dataset.state
-    });
-  };
   render() {
     return (
       <Router>
         <div>
-          <Navigation navigation={this.state.navigation} />
-          {/*<Carousel />*/}
-          <div className="container">
-            <ul>
-              <li>
-                <Link onClick={this.changeState} data-state="main" to="/">
-                  MMPStudio
-                </Link>
-              </li>
-              <li>
-                <Link
-                  onClick={this.changeState}
-                  data-state="fb"
-                  to="/fotobudka"
-                >
-                  Fotobudka
-                </Link>
-              </li>
-              <li>
-                <Link
-                  onClick={this.changeState}
-                  data-state="jubiler"
-                  to="/jubiler"
-                >
-                  Jubiler
-                </Link>
-              </li>
-            </ul>
-          </div>
           <Switch>
             <Route exact path="/" component={MMPStudio} />
-            <Route path="/galeria" component={Gallery} />
-            <Route path="/kontakt" component={Contact} />
+            <Route exact path="/galeria" component={Gallery} />
+            <Route exact path="/kontakt" component={Contact} />
+            <Route exact path="/fotobudka" component={Fotobudka} />
+            <Route exact path="/jubiler" component={Jubiler} />
             <Route
               exact
               path="/fotobudka/kontakt"
               component={FotobudkaContact}
             />
             <Route exact path="/jubiler/galeria" component={JubilerGallery} />
-            <Route path="/fotobudka" component={Fotobudka} />
-            <Route path="/jubiler" component={Jubiler} />
-            <Route exact path="/not-found" component={NotFound} />
-            <Redirect from="*" to="/not-found" />
+            <Redirect from="*" to="/" />
           </Switch>
         </div>
       </Router>
     );
   }
-  componentDidMount() {
-    console.log(this.state.navigation);
-    this.setState({
-      navigation: "main"
-    });
-  }
-  componentWillUnmount() {
-    this.setState({
-      navigation: "main"
-    });
-  }
-  // componentDidUpdate() {
-  //   if(window.location.href.indexOf('fotobudka') > 0) {
-  //     console.log('hi');
-  //   }
-  // }
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));

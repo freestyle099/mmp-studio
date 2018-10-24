41dd9045815ffe1d1ba88cbf191423926b9e208c
freestyle09 <jozefrzadkosz09@gmail.com>
2018-10-24 18:19:02 +0200

    done logo section

--- client/src/index.js
+++ client/src/index.js
@@ -25,27 +25,39 @@ class Gallery extends React.Component {
   }
 }
 
-
 // Fotobudka
 class Fotobudka extends React.Component {
   render() {
-    return (
-      <div>
-        Hello Fotobudka
-      </div>
-    );
+    return <div>Hello Fotobudka</div>;
   }
 }
 class FotobudkaContact extends React.Component {
   render() {
-    return (
-      <div>
-        Hello Fotobudka Kontakt
-      </div>
-    );
+    return <div>Hello Fotobudka Kontakt</div>;
   }
 }
 
+// Jubiler
+
+class Jubiler extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>Jubiler Main</div>;
+  }
+}
+
+class JubilerGallery extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>Jubiler Gallery</div>;
+  }
+}
 
 // Navigation
 class Navigation extends React.Component {
@@ -54,49 +66,67 @@ class Navigation extends React.Component {
       <div>
         {this.props.navigation === "main" && (
           <nav>
-            <ul className="container navigation">
-              <li>
-                <Link to="/">Strona Domowa</Link>
-              </li>
-              <li>
-                <Link to="/galeria">Galeria</Link>
-              </li>
-              <li>
-                <Link to="/kontakt">Kontakt</Link>
-              </li>
-            </ul>
+            <div className="nav-wrapper container">
+              <Link className='brand-logo' to="/">
+                <img
+                  src='logo_studio.png'
+                  alt=""
+                />
+              </Link>
+              <ul className="container navigation right">
+                <li>
+                  <Link to="/">Strona Domowa</Link>
+                </li>
+                <li>
+                  <Link to="/galeria">Galeria</Link>
+                </li>
+                <li>
+                  <Link to="/kontakt">Kontakt</Link>
+                </li>
+              </ul>
+            </div>
           </nav>
         )}
         {this.props.navigation === "fb" && (
-          <nav className='fb-nav'>
-            <ul className="container navigation">
-              <li>
-                <Link to="/fotobudka">Fotobudka</Link>
-              </li>
-              <li>
-                <Link to="/fotobudka/kontakt">Kontakt</Link>
-              </li>
-            </ul>
+          <nav className="fb-nav">
+            <div className="nav-wrapper container">
+              <Link className='brand-logo' to="/fotobudka">
+                <img
+                  src='logo_fotobudka.png'
+                  alt=""
+                />
+              </Link>
+              <ul className="container navigation right">
+                <li>
+                  <Link to="/fotobudka">Fotobudka</Link>
+                </li>
+                <li>
+                  <Link to="/fotobudka/kontakt">Kontakt</Link>
+                </li>
+              </ul>
+            </div>
           </nav>
         )}
         {/* Jubiler Navigation */}
         {this.props.navigation === "jubiler" && (
           <nav>
-            <ul className="container navigation">
-              <li>
-                <Link to="/jubiler">Jubiler</Link>
-              </li>
-              <li>
-                <Link to="/jubiler/kontakt">Kontakt</Link>
-              </li>
-            </ul>
+            <div className="nav-wrapper container">
+              <Link to="/">MMPStudio</Link>
+              <ul className="container navigation right">
+                <li>
+                  <Link to="/jubiler">Jubiler</Link>
+                </li>
+                <li>
+                  <Link to="/jubiler/galeria">Galeria</Link>
+                </li>
+              </ul>
+            </div>
           </nav>
         )}
       </div>
     );
   }
 }
-
 
 // Main APP
 class App extends React.Component {
@@ -107,27 +137,50 @@ class App extends React.Component {
     };
   }
 
-  changeState = (e) => {
+  changeState = e => {
     this.setState({
       navigation: e.target.dataset.state
-    })
-  }
+    });
+  };
   render() {
     return (
       <Router>
         <div>
-          <Navigation navigation={this.state.navigation}/>
-          <ul>
-            <li><Link onClick={this.changeState} data-state='main' to='/'>MMPStudio</Link></li>
-            <li><Link onClick={this.changeState} data-state='fb' to='/fotobudka'>Fotobudka</Link></li>
-            <li><Link onClick={this.changeState} data-state='jubiler' to='/jubiler'>Jubiler</Link></li>
-          </ul>
-
+          <Navigation navigation={this.state.navigation} />
+          <div className="container">
+            <ul>
+              <li>
+                <Link onClick={this.changeState} data-state="main" to="/">
+                  MMPStudio
+                </Link>
+              </li>
+              <li>
+                <Link
+                  onClick={this.changeState}
+                  data-state="fb"
+                  to="/fotobudka"
+                >
+                  Fotobudka
+                </Link>
+              </li>
+              <li>
+                <Link
+                  onClick={this.changeState}
+                  data-state="jubiler"
+                  to="/jubiler"
+                >
+                  Jubiler
+                </Link>
+              </li>
+            </ul>
+          </div>
           <Route exact path="/" component={MMPStudio} />
           <Route exact path="/galeria" component={Gallery} />
           <Route exact path="/kontakt" component={Contact} />
           <Route exact path="/fotobudka" component={Fotobudka} />
-          <Route exact path="/fotobudka/kontakt" component={FotobudkaContact} />
+          <Route exact path="/jubiler" component={Jubiler} />{" "}
+          <Route exact path="/fotobudka" component={Fotobudka} />
+          <Route exact path="/jubiler/galeria" component={JubilerGallery} />
         </div>
       </Router>
     );

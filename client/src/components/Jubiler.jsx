c3cd8cb97941fbcb7b68fe8fb37e27934e0a1ffd
freestyle09 <jozefrzadkosz09@gmail.com>
2018-10-31 19:40:03 +0100

    done services

--- client/src/components/Jubiler.jsx
+++ client/src/components/Jubiler.jsx
@@ -19,6 +19,28 @@ export default class Jubiler extends React.Component {
         <JubilerNavigation />
         <Carousel images={this.state.images} />
         <NavigationImages />
+        <div>
+          <h2>Strona Główna</h2>
+          <div className='container'>
+            Zapraszamy do skorzystania z usług naszej pracowni Jubilersko
+            Złotniczej którą prowadzimy od pokoleń w Zakopanem w ofercie
+            posiadamy ponad 200 wzorów ręcznie wykonanych obrączek i przy
+            wybraniu naszych usług Foto/Video oferujemy rabat na ręcznie
+            wykonane obrączki w wysokości -20%.
+          </div>
+          <div className='services'>
+            Pracownia Złotnicza poleca swoje usługi w zakresie:
+
+            Wykonywanie biżuterii złotej z materiałów własnych i powierzonych.
+            Zmniejszanie i powiększanie pierścionków, obrączek.
+            Naprawa starej i współczesnej biżuterii złotej, srebrnej.
+            Naprawa kopert zegarkowych i bransolet.
+            Przewlekanie korali i pereł itp.
+            Sprzedaż biżuterii.
+            Skup złomu złota, srebra i kamieni szlachetnych.
+            Porady fachowe.
+          </div>
+        </div>
       </div>
     );
   }

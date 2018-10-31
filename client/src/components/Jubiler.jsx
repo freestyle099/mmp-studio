551da94296e9e71dc8ef6a620fdbf2907157abb2
freestyle09 <jozefrzadkosz09@gmail.com>
2018-10-31 19:47:00 +0100

    added jub photo

--- client/src/components/Jubiler.jsx
+++ client/src/components/Jubiler.jsx
@@ -21,29 +21,35 @@ export default class Jubiler extends React.Component {
         <NavigationImages />
         <div>
           <h2>Strona Główna</h2>
-          <div className='container'>
-            Zapraszamy do skorzystania z usług naszej pracowni Jubilersko
-            Złotniczej którą prowadzimy od pokoleń w Zakopanem w ofercie
-            posiadamy ponad 200 wzorów ręcznie wykonanych obrączek i przy
-            wybraniu naszych usług Foto/Video oferujemy rabat na ręcznie
-            wykonane obrączki w wysokości -20%.
+          <div className="container jub-info">
+            <div>
+              Zapraszamy do skorzystania z usług naszej pracowni Jubilersko
+              Złotniczej którą prowadzimy od pokoleń w Zakopanem w ofercie
+              posiadamy ponad 200 wzorów ręcznie wykonanych obrączek i przy
+              wybraniu naszych usług Foto/Video oferujemy rabat na ręcznie
+              wykonane obrączki w wysokości -20%.
+            </div>
+            <div>
+              <img src="jubiler.jpg" alt=""/>
+            </div>
           </div>
-          <div className='services'>
-            Pracownia Złotnicza poleca swoje usługi w zakresie:
-
-            Wykonywanie biżuterii złotej z materiałów własnych i powierzonych.
-            Zmniejszanie i powiększanie pierścionków, obrączek.
-            Naprawa starej i współczesnej biżuterii złotej, srebrnej.
-            Naprawa kopert zegarkowych i bransolet.
-            Przewlekanie korali i pereł itp.
-            Sprzedaż biżuterii.
-            Skup złomu złota, srebra i kamieni szlachetnych.
-            Porady fachowe.
+          <div className="services">
+            <div className="container">
+              <h2>Usługi</h2>
+              Pracownia Złotnicza poleca swoje usługi w zakresie: Wykonywanie
+              biżuterii złotej z materiałów własnych i powierzonych.
+              Zmniejszanie i powiększanie pierścionków, obrączek. Naprawa starej
+              i współczesnej biżuterii złotej, srebrnej. Naprawa kopert
+              zegarkowych i bransolet. Przewlekanie korali i pereł itp. Sprzedaż
+              biżuterii. Skup złomu złota, srebra i kamieni szlachetnych. Porady
+              fachowe.
+            </div>
           </div>
         </div>
       </div>
     );
   }
+
   componentDidMount() {
     fetch("/api/jubilers")
       .then(resp => resp.json())
